5067dee0acce4a36c2810762b44e852352d18d43
Lassanie <dgknbarbaros@gmail.com>
2023-01-11 20:05:18 +0300

    form componenti eklendi

---
+++ src/components/Booklist.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Booklist = () => {
+    return  <h2>Kitaplar</h2>;
+};
+
+export default Booklist;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Kitap Düzenleme Uygulaması</h1>
+            <hr />
+            <div className='links'>
+                <NavLink to="/" className="link" activeClassName="active" exact>                    
+                Kitap Listesi
+                </NavLink>
+                <NavLink to="/add" className="link" activeClassName="active">                    
+                Kitap ekle
+                </NavLink>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
